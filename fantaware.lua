e56805acb73ff3793628e35ee4312e8c2ae18203
void <94395605+v9h@users.noreply.github.com>
2022-10-03 21:02:03 +0300

    FIX MY MATH PROBLE<M
    1.7+2.9=4.8

--- fantaware.lua
+++ fantaware.lua
@@ -128,14 +128,14 @@ local section3 = menu3:addSection({
 local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 game.Players.LocalPlayer.Character = nil
 game.Players.LocalPlayer.Character = workspace[game.Players.LocalPlayer.Name]
-wait(1.8)
+wait(1.9)
 for _,v in ipairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
     if v:IsA("Motor6D") and tostring(v) ~= "Neck" then
         local n = v.Parent
         v:Destroy(); n.CFrame = CFrame.new(9e9, 300000, 9e9); task.wait(0/1)
     end
 end
-wait(3.0)
+wait(2.9)
 game.Players.LocalPlayer.Character.Humanoid.Health = 0
 wait(0.1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
