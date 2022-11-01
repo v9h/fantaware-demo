e169614eb0ead208a7b866da4ff68acbf89745bd
void <94395605+v9h@users.noreply.github.com>
2022-11-01 16:19:05 +0200

    Update fantaware.lua

--- fantaware.lua
+++ fantaware.lua
@@ -154,27 +154,6 @@ section3:addButton({
 game.Players.LocalPlayer.Character.Humanoid:SetStateEnabled("Seated", false)
 game.Players.LocalPlayer.Character.Humanoid.Sit = true
         end)
-    section3:addButton({
-            text = 'Crash', 
-            style = 'large'
-        }):bindToEvent('onClick', function()
-local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-game.Players.LocalPlayer.Character = nil
-game.Players.LocalPlayer.Character = workspace[game.Players.LocalPlayer.Name]
-wait(1.9)
-for _,v in ipairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-    if v:IsA("Motor6D") and tostring(v) ~= "Neck" then
-        local n = v.Parent
-        v:Destroy(); n.CFrame = CFrame.new(9e9, 300000, 9e9); task.wait(0/1)
-    end
-end
-wait(2.9)
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-wait(0.1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
-
-game.Players.LocalPlayer.CharacterAdded:Wait():WaitForChild('HumanoidRootPart')['CFrame'] = pos
-        end) -- will probably work with more clothes and repeatedly doing it
             section3:addButton({
             text = 'No Collisions', 
             style = 'large' 
