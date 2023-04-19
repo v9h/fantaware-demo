a86c1685743778c29b0cdccfc8655d45c236bb4f
v9h <94395605+v9h@users.noreply.github.com>
2023-04-19 19:12:46 +0300

    Update fantaware.lua

--- fantaware.lua
+++ fantaware.lua
@@ -142,12 +142,131 @@ game.Players.LocalPlayer.Character.Humanoid.Sit = true
             text = 'No Collisions', 
             style = 'large' 
         }):bindToEvent('onClick', function()
-print('will put this back in when roblox reverts a little thing')
+        if syn then
+            if not game:IsLoaded() then
+    game.IsLoaded:Wait()
+end;
+
+local RunService = game.RunService
+local SpeedWaitBind = Instance.new("BindableEvent")
+for _, v in ipairs({RunService.RenderStepped, RunService.Heartbeat, RunService.Stepped, RunService.PreRender, RunService.PostSimulation, RunService.PreSimulation}) do
+    v.Connect(v, function()
+        return SpeedWaitBind.Fire(SpeedWaitBind, tick())
+    end)
+end
+
+local FastWait = function(Number)
+    if not type(Number) == "number" then
+        return
+    else
+        if Number == 0 or Number == "" or Number == " " then
+            Number = 0
+            SpeedWaitBind.Event:Wait(Number)
+        else
+            SpeedWaitBind.Event:Wait(number)
+        end
+    end
+end
+
+local Players = game:service("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if not LocalPlayer.Character then
+    repeat FastWait() until LocalPlayer.Character
+end;
+
+if not LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
+    repeat FastWait() until LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+end;
+
+local CheckInstance = function(Part)
+    if Part.ClassName:lower():find("part") then
+        local BaseInstance = Part;
+        pcall(function()
+            for i, Part in pairs(LocalPlayer.Character:GetDescendants()) do
+                if Part.ClassName:lower():find("part") then
+                    local NoCollisionConstraint = Instance.new("NoCollisionConstraint", BaseInstance)
+                    NoCollisionConstraint.Enabled = true
+                    NoCollisionConstraint.Part0 = BaseInstance;
+                    NoCollisionConstraint.Part1 = Part;
+                end
+            end
+        end)
+    end
+end;
+
+local Insert = function(Target, Bool)
+    local Start = function()
+        local Inserted = {}
+        if not Target.Character then
+            Target.CharacterAdded:Wait()
+        end;
+        if not Target.Character:FindFirstChildOfClass("Humanoid") then
+            repeat FastWait() until Target.Character:FindFirstChildOfClass("Humanoid")
+        end;
+        spawn(function()
+            pcall(function()
+                for i, v in pairs(Target.Character:GetDescendants()) do
+                    if v:IsA("NoCollisionConstraint") then
+                        v:Destroy()
+                    end
+                end;
+                for i, v in pairs(Target.Character:GetDescendants()) do
+                    CheckInstance(v)
+                end;
+                Target.Character.DescendantAdded:Connect(function(v)
+                    if not table.find(Inserted, v) then
+                        table.insert(Inserted, v)
+                        CheckInstance(v)
+                    end
+                end)
+            end)
+        end)
+    end;
+    pcall(Start)
+    if not Bool then
+        Target.CharacterAdded:Connect(Start)
+    end
+end;
+
+for i, v in pairs(Players:GetPlayers()) do
+    if v ~= LocalPlayer then
+        Insert(v, false)
+    end
+end;
+
+Players.PlayerAdded:Connect(Insert, false)
+
+local Reset = function()
+    if not LocalPlayer.Character then
+        LocalPlayer.CharacterAdded:Wait()
+    end;
+    if not LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
+        repeat FastWait() until LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+    end;
+    for i, v in pairs(Players:GetPlayers()) do
+        if v ~= LocalPlayer then
+            pcall(function()
+                for i, v in pairs(v.Character:GetDescendants()) do
+                    if v.IsA(v, "NoCollisionConstraint") then
+                        v:Destroy()
+                    end
+                end
+            end)
+            Insert(v, true)
+        end
+    end
+end;
+
+LocalPlayer.CharacterAdded:Connect(Reset)
+else
+print('Sorry, this feature is disabled on your exploit for compatibility reasons.')
+end
         end)
          section3:addButton({
             text = 'Respawn', 
-            style = 'large' -- style of the button, can be 'large' or 'small'
-        }):bindToEvent('onClick', function() -- Call a function when clicked
+            style = 'large' 
+        }):bindToEvent('onClick', function()
 local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 game.Players.LocalPlayer.Character = nil
 game.Players.LocalPlayer.Character = workspace[game.Players.LocalPlayer.Name]
@@ -166,8 +285,8 @@ game.Players.LocalPlayer.CharacterAdded:Wait():WaitForChild('HumanoidRootPart')[
     })
              section4:addButton({
             text = 'Ak-47', 
-            style = 'large' -- style of the button, can be 'large' or 'small'
-        }):bindToEvent('onClick', function() -- Call a function when clicked
+            style = 'large'
+        }):bindToEvent('onClick', function() 
 local plr = game:GetService("Players").LocalPlayer; plr.Character.Humanoid:UnequipTools()
  local Tools = plr.Backpack:GetChildren()
 
@@ -233,8 +352,8 @@ local plr = game:GetService("Players").LocalPlayer; plr.Character.Humanoid:Unequ
         end)
                      section4:addButton({
             text = 'Pickaxe', 
-            style = 'large' -- style of the button, can be 'large' or 'small'
-        }):bindToEvent('onClick', function() -- Call a function when clicked
+            style = 'large' 
+        }):bindToEvent('onClick', function() 
 local plr = game:GetService("Players").LocalPlayer; plr.Character.Humanoid:UnequipTools()
  local Tools = plr.Backpack:GetChildren()
 
